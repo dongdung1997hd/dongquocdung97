cd07363f590792199eeace3e85d592216913f5d2
dung97 <dongdung97.hd@gmail.com>
2019-12-26 22:07:26 +0700

    code review dt

---
+++ review_intro.js
@@ -0,0 +1,9 @@
+var a = document.getElementById('icon-search');
+
+a.addEventListener('click', function () {
+    if (document.getElementById('search-form-top').style.display == 'none' || document.getElementById('search-form-top').style.display == '') {
+        document.getElementById('search-form-top').style.display = 'block';
+    } else {
+        document.getElementById('search-form-top').style.display = 'none';
+    }
+}); 
