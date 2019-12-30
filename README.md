96e7f63b06dd2be922f15b3927631ad8ce186872
dung97 <dongdung97.hd@gmail.com>
2019-12-30 22:16:23 +0700

    demo frontend

--- README.md
+++
@@ -1,1 +0,0 @@
-# dongquocdung97
--- review_intro.js
+++ review_intro.js
@@ -6,4 +6,17 @@ a.addEventListener('click', function () {
     } else {
         document.getElementById('search-form-top').style.display = 'none';
     }
-}); 
+});
+
+window.onscroll = function() {myFunction()};
+
+var relative = document.getElementById("relative");
+var sticky = relative.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    relative.classList.add("sticky")
+  } else {
+    relative.classList.remove("sticky");
+  }
+}
